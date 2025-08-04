90d9213a291eede9c027e07ed35f14772892f96f
Joseph Plahitko <jmplahitko@gmail.com>
2025-08-03 22:52:10 -0400

    Cleared out dashboard for now until there's a meaningful design

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -6,27 +6,14 @@
 				<TimeframeSelect />
 			</div>
 		</div>
-		<template v-for="sportName in currentSportNames.toSorted()">
-			<h3 class="text-lg font-medium text-white">{{ sportName }}</h3>
-			<div class="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-				<div v-for="workout in workouts.filter(workout => workout.sport_name === sportName).toSorted((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime())" :key="workout.id"
-					class="bg-gray-600 rounded-lg p-4">
-					<p>{{ workout.sport_name }}</p>
-					<p>{{ workout.start }}</p>
-					<p>{{ workout.end }}</p>
-					<p>{{ workout.score.strain }}</p>
-				</div>
-			</div>
-		</template>
 	</div>
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-	// middleware: 'auth'
-});
+definePageMeta({});
 
 const { user } = useUserSession();
 const { workouts, currentSportNames } = useWorkouts();
+const { cycles } = useCycles();
 
 </script>
