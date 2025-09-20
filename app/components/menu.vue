f572b625334369b22a6f415ffd0951a78587609a
Joseph Plahitko <jmplahitko@gmail.com>
2025-09-19 21:51:09 -0400

    Updated menu icons to lucide

--- app/components/menu.vue
+++ app/components/menu.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="bg-steel-800 w-[255px]">
-		<p class="text-sm font-medium p-2">WHOOP Workout Planner</p>
+		<p class="text-sm font-medium text-muted pt-4 pl-4">WHOOP Workout Planner</p>
 		<UNavigationMenu orientation="vertical" :items="items" variant="pill" highlight-color="neutral" :ui="{
 			root: 'flex flex-col gap-1 w-[255px] p-2',
 			list: 'flex flex-col gap-1',
 			link: 'text-sm font-medium rounded-[6px] text-white hover:text-white before:rounded-[6px]',
-			linkLeadingIcon: 'text-xs font-medium text-white hover:text-white',
+			linkLeadingIcon: 'w-4 h-4 font-medium text-white hover:text-white',
 		}" />
 	</div>
 </template>
@@ -17,32 +17,32 @@ const items: NavigationMenuItem[] = [
 	{
 		label: 'Dashboard',
 		to: '/dashboard',
-		icon: 'i-heroicons-home'
+		icon: 'i-lucide-home'
 	},
 	{
 		label: 'Weekly Planner',
 		to: '/weekly-planner',
-		icon: 'i-heroicons-calendar'
+		icon: 'i-lucide-calendar'
 	},
 	{
 		label: 'Workout Library',
 		to: '/workout-library',
-		icon: 'i-heroicons-bolt'
+		icon: 'i-lucide-zap'
 	},
 	{
 		label: 'Workout Tracker',
 		to: '/workout-tracker',
-		icon: 'i-heroicons-fire'
+		icon: 'i-lucide-dumbbell'
 	},
 	{
 		label: 'Activity Overview',
 		to: '/activity-overview',
-		icon: 'i-heroicons-chart-bar'
+		icon: 'i-lucide-chart-column'
 	},
 	{
 		label: 'Settings',
 		to: '/settings',
-		icon: 'i-heroicons-cog'
+		icon: 'i-lucide-settings'
 	}
 ]
 </script>
