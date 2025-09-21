1d2fdca1230e0eeccc722a0434f22a16ad55afb7
Joseph Plahitko <jmplahitko@gmail.com>
2025-09-21 15:50:42 -0400

    Added orange and fleshed out palette on home route.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,117 @@
 <template>
-	<div class="flex flex-col items-center justify-center h-screen">
-		<div class="bg-steel-900 w-16 h-16">900</div>
-		<div class="bg-steel-800 w-16 h-16">800</div>
-		<div class="bg-steel-700 w-16 h-16">700</div>
-		<div class="bg-steel-600 w-16 h-16">600</div>
-		<div class="bg-steel-500 w-16 h-16">500</div>
-		<div class="bg-steel-400 w-16 h-16">400</div>
-		<div class="bg-steel-300 w-16 h-16">300</div>
-		<div class="bg-steel-200 w-16 h-16"><span class="text-steel-900">200</span></div>
-		<div class="bg-steel-100 w-16 h-16"><span class="text-steel-900">100</span></div>
-		<div class="bg-steel-50 w-16 h-16"><span class="text-steel-900">50</span></div>
+	<div class="flex">
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-blue-900 w-16 h-16">900</div>
+			<div class="bg-blue-800 w-16 h-16">800</div>
+			<div class="bg-blue-700 w-16 h-16">700</div>
+			<div class="bg-blue-600 w-16 h-16">600</div>
+			<div class="bg-blue-500 w-16 h-16">500</div>
+			<div class="bg-blue-400 w-16 h-16">400</div>
+			<div class="bg-blue-300 w-16 h-16">300</div>
+			<div class="bg-blue-200 w-16 h-16"><span class="text-blue-900">200</span></div>
+			<div class="bg-blue-100 w-16 h-16"><span class="text-blue-900">100</span></div>
+			<div class="bg-blue-50 w-16 h-16"><span class="text-blue-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-sky-900 w-16 h-16">900</div>
+			<div class="bg-sky-800 w-16 h-16">800</div>
+			<div class="bg-sky-700 w-16 h-16">700</div>
+			<div class="bg-sky-600 w-16 h-16">600</div>
+			<div class="bg-sky-500 w-16 h-16">500</div>
+			<div class="bg-sky-400 w-16 h-16">400</div>
+			<div class="bg-sky-300 w-16 h-16">300</div>
+			<div class="bg-sky-200 w-16 h-16"><span class="text-sky-900">200</span></div>
+			<div class="bg-sky-100 w-16 h-16"><span class="text-sky-900">100</span></div>
+			<div class="bg-sky-50 w-16 h-16"><span class="text-sky-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-teal-900 w-16 h-16">900</div>
+			<div class="bg-teal-800 w-16 h-16">800</div>
+			<div class="bg-teal-700 w-16 h-16">700</div>
+			<div class="bg-teal-600 w-16 h-16">600</div>
+			<div class="bg-teal-500 w-16 h-16">500</div>
+			<div class="bg-teal-400 w-16 h-16">400</div>
+			<div class="bg-teal-300 w-16 h-16">300</div>
+			<div class="bg-teal-200 w-16 h-16"><span class="text-teal-900">200</span></div>
+			<div class="bg-teal-100 w-16 h-16"><span class="text-teal-900">100</span></div>
+			<div class="bg-teal-50 w-16 h-16"><span class="text-teal-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-green-900 w-16 h-16">900</div>
+			<div class="bg-green-800 w-16 h-16">800</div>
+			<div class="bg-green-700 w-16 h-16">700</div>
+			<div class="bg-green-600 w-16 h-16">600</div>
+			<div class="bg-green-500 w-16 h-16">500</div>
+			<div class="bg-green-400 w-16 h-16">400</div>
+			<div class="bg-green-300 w-16 h-16">300</div>
+			<div class="bg-green-200 w-16 h-16"><span class="text-green-900">200</span></div>
+			<div class="bg-green-100 w-16 h-16"><span class="text-green-900">100</span></div>
+			<div class="bg-green-50 w-16 h-16"><span class="text-green-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-yellow-900 w-16 h-16">900</div>
+			<div class="bg-yellow-800 w-16 h-16">800</div>
+			<div class="bg-yellow-700 w-16 h-16">700</div>
+			<div class="bg-yellow-600 w-16 h-16">600</div>
+			<div class="bg-yellow-500 w-16 h-16">500</div>
+			<div class="bg-yellow-400 w-16 h-16">400</div>
+			<div class="bg-yellow-300 w-16 h-16">300</div>
+			<div class="bg-yellow-200 w-16 h-16"><span class="text-yellow-900">200</span></div>
+			<div class="bg-yellow-100 w-16 h-16"><span class="text-yellow-900">100</span></div>
+			<div class="bg-yellow-50 w-16 h-16"><span class="text-yellow-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-orange-900 w-16 h-16">900</div>
+			<div class="bg-orange-800 w-16 h-16">800</div>
+			<div class="bg-orange-700 w-16 h-16">700</div>
+			<div class="bg-orange-600 w-16 h-16">600</div>
+			<div class="bg-orange-500 w-16 h-16">500</div>
+			<div class="bg-orange-400 w-16 h-16">400</div>
+			<div class="bg-orange-300 w-16 h-16">300</div>
+			<div class="bg-orange-200 w-16 h-16"><span class="text-orange-900">200</span></div>
+			<div class="bg-orange-100 w-16 h-16"><span class="text-orange-900">100</span></div>
+			<div class="bg-orange-50 w-16 h-16"><span class="text-orange-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-red-900 w-16 h-16">900</div>
+			<div class="bg-red-800 w-16 h-16">800</div>
+			<div class="bg-red-700 w-16 h-16">700</div>
+			<div class="bg-red-600 w-16 h-16">600</div>
+			<div class="bg-red-500 w-16 h-16">500</div>
+			<div class="bg-red-400 w-16 h-16">400</div>
+			<div class="bg-red-300 w-16 h-16">300</div>
+			<div class="bg-red-200 w-16 h-16"><span class="text-red-900">200</span></div>
+			<div class="bg-red-100 w-16 h-16"><span class="text-red-900">100</span></div>
+			<div class="bg-red-50 w-16 h-16"><span class="text-red-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-slate-900 w-16 h-16">900</div>
+			<div class="bg-slate-800 w-16 h-16">800</div>
+			<div class="bg-slate-700 w-16 h-16">700</div>
+			<div class="bg-slate-600 w-16 h-16">600</div>
+			<div class="bg-slate-500 w-16 h-16">500</div>
+			<div class="bg-slate-400 w-16 h-16">400</div>
+			<div class="bg-slate-300 w-16 h-16">300</div>
+			<div class="bg-slate-200 w-16 h-16"><span class="text-slate-900">200</span></div>
+			<div class="bg-slate-100 w-16 h-16"><span class="text-slate-900">100</span></div>
+			<div class="bg-slate-50 w-16 h-16"><span class="text-slate-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-gray-900 w-16 h-16">900</div>
+			<div class="bg-gray-800 w-16 h-16">800</div>
+			<div class="bg-gray-700 w-16 h-16">700</div>
+			<div class="bg-gray-600 w-16 h-16">600</div>
+			<div class="bg-gray-500 w-16 h-16">500</div>
+			<div class="bg-gray-400 w-16 h-16">400</div>
+			<div class="bg-gray-300 w-16 h-16">300</div>
+			<div class="bg-gray-200 w-16 h-16"><span class="text-gray-900">200</span></div>
+			<div class="bg-gray-100 w-16 h-16"><span class="text-gray-900">100</span></div>
+			<div class="bg-gray-50 w-16 h-16"><span class="text-gray-900">50</span></div>
+		</div>
+		<div class="flex flex-col items-center justify-center h-screen">
+			<div class="bg-white w-16 h-16"><span class="text-black">White</span></div>
+			<div class="bg-black w-16 h-16">Black</div>
+		</div>
 	</div>
 </template>
 
